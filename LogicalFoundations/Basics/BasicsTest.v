2770225ad17081a365d0064c5374752212549f6d
DendyLiJunyi <165771014+DendyLiJunyi@users.noreply.github.com>
2025-12-18 23:59:41 +0000

    Update BasicsTest.v

--- LogicalFoundations/Basics/BasicsTest.v
+++ LogicalFoundations/Basics/BasicsTest.v
@@ -23,3 +23,23 @@ Example test_nandb4: (nandb true true) = false.
 Proof. simpl. reflexivity. Qed.
 
 (* * Exercise : andb3 * *)
+
+Definition andb3 (b1 : bool) (b2 : bool) (b3 : bool) :=
+  if b1 then 
+    if b2 then b3
+    else false
+  else false.
+
+Example test_andb31: (andb3 true true true) = true.
+
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb32: (andb3 false true true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb33: (andb3 true false true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb34: (andb3 true true false) = false.
+Proof. simpl. reflexivity. Qed.
+
