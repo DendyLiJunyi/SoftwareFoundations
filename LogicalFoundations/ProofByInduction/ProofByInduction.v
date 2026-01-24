5fe752e044d992ccf4054fd90dbbc374831b3e7d
DendyLiJunyi <DendyLiJunyi@github.com>
2026-01-24 09:35:36 +0000

    update

---
+++ LogicalFoundations/ProofByInduction/ProofByInduction.v
@@ -0,0 +1,113 @@
+From LogicalFoundations Require Export Basics.
+
+Theorem add_0_r_firsttry : forall n : nat, n + 0 = n.
+Proof.
+  intros n.
+  simpl.
+Abort.
+
+Theorem add_0_r_secondtry : forall n : nat, n + 0 = n.
+Proof.
+  intros n. destruct n as [| n'] eqn:E.
+  - reflexivity.
+  - simpl.
+Abort.
+
+(** We can further destruct n', since n' can be arbitrarily large, this doesn't work.
+    The more powerful reasoning principle we need is induction.
+    - show P(0) holds
+    - show for any n', if P(n') holds, then so does P(S n')
+    - conclude that P(n) holds for all n.*)
+
+Theorem add_0_r : forall n : nat, n + 0 = n.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - reflexivity.
+  - simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem minus_n_n : forall n, minus n n = 0.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - reflexivity.
+  - simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem mul_0_r : forall n : nat, n * 0 = 0.
+Proof.
+  induction n as [| n' IHn'].
+  - reflexivity.
+  - simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem plus_n_Sm : forall n m : nat,
+  S (n + m) = n + (S m).
+Proof.
+  induction n as [| n' IHn'].
+  - induction m as [| m' IHm'].
+    -- reflexivity.
+    -- reflexivity.
+  - induction m as [| m' IHm'].
+    -- simpl. rewrite -> IHn'. reflexivity.
+    -- simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem add_assoc : forall n m p : nat,
+  n + (m + p) = (n + m) + p.
+Proof.
+  induction n as [| n' IHn'].
+  - induction m as [| m'].
+    -- induction p as [| p'].
+       --- reflexivity.
+       --- reflexivity.
+    -- induction p as [| p'].
+       --- reflexivity.
+       --- reflexivity.
+  - induction m as [| m'].
+    -- induction p as [| p' IHp'].
+       --- simpl. rewrite <- IHn'. reflexivity.
+       --- simpl. rewrite <- IHn'. reflexivity.
+    -- induction p as [| p' IHp'].
+       --- simpl. rewrite <- IHn'. reflexivity.
+       --- simpl. rewrite <- IHn'. reflexivity.
+Qed.
+
+Fixpoint double (n : nat) :=
+  match n with
+  | 0 => 0
+  | S n' => S (S (double n'))
+  end.
+
+Lemma double_plus : forall n, double n = n + n.
+Proof.
+  induction n as [| n' IHn'].
+  - reflexivity.
+  - simpl. rewrite -> IHn'. rewrite -> plus_n_Sm. reflexivity.
+Qed.
+
+Theorem eqb_refl : forall n : nat, (n =? n) = true.
+Proof.
+  induction n as [| n' IHn'].
+  - reflexivity.
+  - simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem negb_negb : forall a : bool, negb (negb a) = a.
+Proof.
+  intro a.
+  destruct a as [|] eqn:E.
+  - reflexivity.
+  - reflexivity.
+Qed.
+
+Theorem even_S : forall n : nat, even (S n) = negb (even n).
+Proof.
+  induction n as [| n' IHn'].
+  - reflexivity.
+  - rewrite -> IHn'. simpl. rewrite -> negb_negb. reflexivity.
+Qed.
+
+(** Large proofs are often broken into a sequence of theorems, with later proofs referring to earier theorem.*)
+
+
+
