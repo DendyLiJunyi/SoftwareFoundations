b41045b4f1f02831c312f11c014878953fd66062
DendyLiJunyi <DendyLiJunyi@github.com>
2026-03-05 23:54:30 +0800

    update

--- LogicalFoundations/Tactics/Tactics.v
+++ LogicalFoundations/Tactics/Tactics.v
@@ -160,5 +160,122 @@ Proof.
   rewrite -> Hxy. symmetry. apply Hyz.
 Qed.
 
-(** rewrite -> is replace left by right. *)
+(** rewrite -> is replace left by right.
+   
+   disjointness says two terms beginning with different constructors can never be equal. *)
+
+Theorem discriminate_ex1 : forall (n m : nat),
+  false = true ->
+  n = m.
+Proof.
+  intros n m contra.
+  discriminate contra.
+Qed.
+
+Theorem discriminate_ex2 : forall (n : nat),
+  S n = 0 ->
+  2 + 2 = 5.
+Proof.
+  intros n contra.
+  discriminate contra.
+Qed.
+
+(** These examples are principle of explosion, a contradictory hypothesis entails anything. 
+
+   When the discriminate happens the theorem is somehow nonesense. *)
+
+Example discriminate_ex3 :
+  forall (X : Type) (x y z : X) (l j : list X),
+  x :: y :: l = [] ->
+  x = z.
+Proof.
+  intros X x y z l HX contra.
+  discriminate contra.
+Qed.
+
+(** Use discriminate to make connection. *)
+Theorem eqb_0_l : forall n,
+  0 =? n = true -> n = 0.
+Proof.
+  intros n.
+  destruct n as [| n'].
+  - (* n = 0 *)
+    intros H. reflexivity.
+  - (* n = S n' *)
+    intros contra. discriminate contra.
+Qed.
+
+Theorem f_equal : forall (A B : Type) (f : A -> B) (x y : A),
+  x = y -> f x = f y.
+Proof.
+  intros A B f x y eq.
+  rewrite <- eq.
+  reflexivity.
+Qed.
+
+Theorem eq_implies_succ_equal : forall (n m : nat),
+  n = m -> S n = S m.
+Proof. intros n m H. apply f_equal. apply H. Qed.
+
+(** f_equal tactic *)
+
+Theorem eq_implies_succ_equal' : forall (n m : nat),
+  n = m -> S n = S m.
+Proof.
+  intros n m H.
+  f_equal.
+  apply H.
+Qed.
+
+(** * Using Tactics on Hypotheses * **)
+
+Theorem S_inj : forall (n m : nat) (b : bool),
+  ((S n) =? (S m)) = b ->
+  (n =? m) = b.
+Proof.
+  intros n m b H.
+  simpl in H.
+  apply H.
+Qed.
+
+(** apply L in H gives us a form of "forward reasoning" *)
+Theorem silly4 : forall (n m p q : nat),
+  (n = m -> p = q) ->
+  m = n ->
+  q = p.
+Proof.
+  intros n m p q H1 H2.
+  symmetry in H2.
+  apply H1 in H2.
+  symmetry in H2.
+  apply H2.
+Qed.
+
+(** Forward reasoning is build a theorem exactly match the goal.
+   Backward reasoning is solve the goal. *)
+
+(** * Specializing Hypotheses * **)
+
+(** specialize = assert and apply *)
+Theorem specialize_example : forall n,
+  (forall m, m * n = 0) -> n = 0.
+Proof.
+  intros n H.
+  specialize H with (m := 1).
+  Search (1 * _ = _).
+  rewrite -> Nat.mul_1_l in H.
+  apply H.
+Qed.
+
+Theorem specialize_example' : forall n,
+  (forall m, m * n = 0) -> n = 0.
+Proof.
+  intros n H.
+  assert (H2 : 1 * n = 0). { apply H. }
+  rewrite -> Nat.mul_1_l in H2.
+  apply H2.
+Qed.
+
+(** A -> B is replace A with B *)
+
 
